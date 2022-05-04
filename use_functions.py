ddb6f636e7cda11d200a2f03c09dbac3b7df0602
kvoloshenko <kvoloshenko@gmail.com>
2022-05-04 22:42:53 +0300

    Updated

--- use_functions.py
+++ use_functions.py
@@ -33,6 +33,45 @@
 
 Для реализации основного меню можно использовать пример ниже или написать свой
 """
+account = 0
+purchases = []
+
+def purchase_get():
+    global purchases
+    if len(purchases) == 0:
+        print('Список покупок пуст')
+    else:
+        print ('Список совершенных покупок:')
+        for purchase in purchases:
+            print('  ', purchase['purchase'], ' :',purchase['sum'])
+
+def purchase_add():
+    while True:
+        answer = input('Введите сумму покупки: ')
+        if answer.isdigit(): break
+    sum = int(answer)
+    global account
+    if sum <= account:
+        account -= sum
+        purchase = input('Введите описание покупки: ')
+        global purchases
+        purchases.append({'purchase': purchase, 'sum': sum})
+        #print(purchases)
+    else: print ('Недостаточно средств на счету')
+
+def account_add():
+    while True:
+        answer = input('Введите сумму пополнения счета: ')
+        if answer.isdigit(): break
+
+    sum = int(answer)
+    global account
+    account += sum
+
+
+def account_get():
+    print('Сейчас на счету: ', account)
+
 
 while True:
     print('1. пополнение счета')
@@ -40,13 +79,16 @@ while True:
     print('3. история покупок')
     print('4. выход')
 
-    choice = input('Выберите пункт меню')
+    choice = input('Выберите пункт меню : ')
     if choice == '1':
-        pass
+        account_add()
+        account_get()
     elif choice == '2':
-        pass
+        purchase_add()
+        account_get()
     elif choice == '3':
-        pass
+        purchase_get()
+        account_get()
     elif choice == '4':
         break
     else:
